caca91ef96ed19c14c3ab2e0dbe00e1cd9a309b8
TakeruOgasawara <119379501+TakeruOgasawara@users.noreply.github.com>
2023-11-21 14:53:23 +0900

    武器ごとにeffectを追加

--- 00_team/code/itemWeapon.h
+++ 00_team/code/itemWeapon.h
@@ -34,6 +34,7 @@ private:
 	void Interact(CObject* pObj);
 	void ApplyEffect(CPlayer *pPlayer);
 	void CollisionField(void);
+	void BindEffect(D3DXVECTOR3 pos, D3DXVECTOR3 move);
 
 	CWeapon::TYPE m_type;
 };
