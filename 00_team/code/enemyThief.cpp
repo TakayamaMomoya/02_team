749a2dbe143df441f72d6ab54f2fd8dd6709dcbe
TakayamaMomoya <119379245+TakayamaMomoya@users.noreply.github.com>
2023-12-06 12:05:56 +0900

    敵に歩きモーションをさせるように設定

--- 00_team/code/enemyThief.cpp
+++ 00_team/code/enemyThief.cpp
@@ -20,13 +20,16 @@
 #include "enemyManager.h"
 #include "debrisSpawner.h"
 #include "block.h"
+#include "universal.h"
+#include "rocket.h"
+#include "motion.h"
 
 //=====================================================
 // コンストラクタ
 //=====================================================
 CEnemyThief::CEnemyThief()
 {
-
+	m_bTakeRepair = false;
 }
 
 //=====================================================
@@ -44,6 +47,14 @@ HRESULT CEnemyThief::Init(void)
 {
 	// 継承クラスの初期化
 	CEnemy::Init();
+
+	// 歩きモーションに設定
+	CMotion *pBody = GetBody();
+
+	if (pBody != nullptr)
+	{
+		pBody->SetMotion(MOTION_WALK);
+	}
 
 	return S_OK;
 }
@@ -83,6 +94,63 @@ void CEnemyThief::Update(void)
 			SetMotion(MOTION_DEATH);
 		}
 	}
+
+	// ロケットを追う処理
+	ChaseRocket();
+}
+
+//=====================================================
+// ロケットを追う処理
+//=====================================================
+void CEnemyThief::ChaseRocket(void)
+{
+	// ゲーム状態によって追跡させない
+	CGame *pGame = CGame::GetInstance();
+
+	if (pGame != nullptr)
+	{
+		CGame::STATE state = pGame->GetState();
+
+		if (state != CGame::STATE::STATE_NORMAL)
+		{
+			return;
+		}
+	}
+
+	// 目標位置の取得
+	D3DXVECTOR3 posTarget = { 0.0f,0.0f,0.0f };
+
+	CRocket *pRocket = CRocket::GetInstance();
+
+	if (pRocket != nullptr)
+	{
+		posTarget = pRocket->GetPosition();
+	}
+
+	// 移動量の設定
+	D3DXVECTOR3 pos = GetPosition();
+	D3DXVECTOR3 vecDiff = posTarget - pos;
+	D3DXVECTOR3 move = GetMove();
+
+	D3DXVec3Normalize(&vecDiff, &vecDiff);
+
+	float fSpeed = GetSpeed();
+
+	vecDiff *= fSpeed;
+
+	move += vecDiff;
+
+	SetMove(move);
+
+	// 向きを目標方向に補正
+	float fAngleDist = atan2f(vecDiff.x, vecDiff.z);
+	D3DXVECTOR3 rot = GetRot();
+
+	fAngleDist += D3DX_PI;
+
+	universal::FactingRot(&rot.y, fAngleDist, 0.1f);
+
+	SetRot(rot);
 }
 
 //=====================================================
